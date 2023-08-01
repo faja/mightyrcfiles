ea2fba4d217233b889931b1c9d6bd53ab6b81992
faja <mcabaj@gmail.com>
2023-08-01 20:55:56 +0200

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -634,8 +634,9 @@ vim.keymap.set("n", "gq", "<cmd>lua vim.diagnostic.setqflist()<cr>", keymapopts)
 -- }}}
 
 -- autocmd {{{
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {pattern='*.nomadtpl', command='set syntax=hcl'})
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {pattern='*.nomad.j2', command='set syntax=hcl'})
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {pattern='*.nomadtpl', command='set filetype=hcl'})
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {pattern='*.nomad.j2', command='set filetype=hcl'})
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {pattern='docker-compose.y*ml.*', command='set filetype=docker-compose'})
 vim.api.nvim_create_autocmd({"BufWritePre"}, {pattern='*', command=[[%s/\s\+$//e]]}) -- remove trailing white spaces
 -- }}}
 
