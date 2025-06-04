79100bfe20717e7d086796d9dd55cc4e806bfc7e
faja <mcabaj@gmail.com>
2025-06-04 23:58:58 +0200

    added mini.indentscope

--- config/nvim-2025/lua/plugins/indent.lua
+++ config/nvim-2025/lua/plugins/indent.lua
@@ -1,15 +1,30 @@
 return {
-	-- https://github.com/lukas-reineke/indent-blankline.nvim
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	-- :help ibl.config (but once the plugin is loaded)
-	opts = {
-		enabled = false,
-		indent = {
-			char = { "|", "¦", "┆", "┊" },
+	{
+		-- https://github.com/lukas-reineke/indent-blankline.nvim
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		-- :help ibl.config (but once the plugin is loaded)
+		opts = {
+			enabled = false,
+			indent = {
+				char = { "|", "¦", "┆", "┊" },
+			},
+		},
+		keys = {
+			{ "<leader>I", ":IBLToggle<cr>" },
 		},
 	},
-	keys = {
-		{ "<leader>I", ":IBLToggle<cr>" },
+	{
+		-- https://github.com/echasnovski/mini.indentscope
+		"echasnovski/mini.indentscope",
+		version = false,
+		opts = {
+			draw = {
+				delay = 0,
+				animation = function()
+					return 0
+				end,
+			},
+		},
 	},
 }
