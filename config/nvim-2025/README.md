6bbafb541e20ac8bc46c8eed2247862ddf9ae53b
faja <mcabaj@gmail.com>
2025-06-11 22:54:48 +0200

    nvim 2025!

---
+++ config/nvim-2025/README.md
@@ -0,0 +1,27 @@
+# TODO
+
+files to go through and check for TODOs
+
+├── lua
+│   ├── core
+│   │   ├── autocmd.lua
+│   │   ├── colors.lua
+│   │   ├── keymaps.lua
+│   │   └── lesssgo.lua
+│   ├── lazyyy.lua
+│   └── plugins
+│       ├── autocompletion.lua
+│       ├── colorschemes.lua
+│       ├── comments.lua
+│       ├── common.lua
+│       ├── format.lua
+│       ├── fzf-lua.lua
+│       ├── git.lua
+│       ├── harpoon.lua
+│       ├── indent.lua
+│       ├── lsp.lua
+│       ├── markdown.lua
+│       ├── nvim-tree.lua
+│       ├── statusline.lua
+│       ├── symlink.lua
+│       └── treesitter.lua
--- config/nvim-2025/lua/plugins/common.lua
+++ config/nvim-2025/lua/plugins/common.lua
@@ -1,37 +1,36 @@
 return {
-	-- --
-	--[[
-        helpers first
-    --]]
 	{
+		-- https://github.com/nvim-lua/plenary.nvim
 		-- bunch of lua functions that other plugins are using, eg telescope
-		"nvim-lua/plenary.nvim", -- https://github.com/nvim-lua/plenary.nvim
+		"nvim-lua/plenary.nvim",
 	},
-	-- {
-	--     WARN: I wonder if I really need this
-	--     WARN: testing with disabled
-	--     -- language pack for syntax and filetypes
-	--     "sheerun/vim-polyglot" -- https://github.com/sheerun/vim-polyglot
-	-- },
 	{
+		-- https://github.com/sheerun/vim-polyglot
+		-- language pack for syntax and filetypes
+		"sheerun/vim-polyglot",
+	},
+	{
+		-- https://github.com/nvim-tree/nvim-web-devicons
 		-- icons
 		-- please install nerdfonts first: https://www.nerdfonts.com/
-		"nvim-tree/nvim-web-devicons", -- https://github.com/nvim-tree/nvim-web-devicons
+		"nvim-tree/nvim-web-devicons",
 		opts = {},
 	},
-
-	--[[
-        real plugins
-    --]]
-	{ "tpope/vim-surround" }, -- https://github.com/tpope/vim-surround
-	{ "tpope/vim-sleuth" }, -- https://github.com/tpope/vim-sleuth, bundled in vim-polyglot, hence disabled
 	{
+		-- https://github.com/tpope/vim-surround
+		"tpope/vim-surround",
+	},
+	-- { sleuth optimized version is bundled in vim-polyglot, hence disabled
+	-- 	-- https://github.com/tpope/vim-sleuth,
+	-- 	"tpope/vim-sleuth",
+	-- },
+	{
+		-- https://github.com/szw/vim-maximizer
 		-- split window maximizer
-		"szw/vim-maximizer", -- https://github.com/szw/vim-maximizer
+		"szw/vim-maximizer",
 		lazy = true,
 		keys = {
 			{ "<leader>sm", ":MaximizerToggle<cr>" },
 		},
 	},
-	{},
 }
